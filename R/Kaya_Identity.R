b00e9b2e339327428e52a31defd14f5a8022b7ca
alex he <hebailanc@outlook.com>
2020-11-12 09:53:25 +0100

    add unit Test

--- R/Kaya_Identity.R
+++ R/Kaya_Identity.R
@@ -8,11 +8,19 @@
 #' This number depends on the energy mix used (coal, solar, . . . ).
 #' @return numeric number co2 as Kaya Identity
 #' examples Kaya_Equation(pop=82.4,gdp=44,enInt = 5,carbInt = 0.05)
+#' @import checkmate
+#' @import testthat
 
+library(checkmate)
+library(testthat)
+Kaya_Equation <- function(pop=82.4,gdp=44,enInt=5,carbInt=0.05){
+  assert_numeric(c(pop,gdp,enInt,carbInt),lower=0)
 
-Kaya_Equation <- function(pop,gdp,enInt,carbInt){
   return( co2=pop*gdp*enInt*carbInt)
 }
 
 
+# Unit Test
+expect_that(Kaya_Equation(pop=82.4,gdp=44,enInt=5,carbInt=0.05), equals(906.4))
+expect_error(c(Kaya_Equation(pop = -1),Kaya_Equation(gdp=-1)))
 
